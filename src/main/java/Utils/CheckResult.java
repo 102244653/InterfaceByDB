4c46bb08046d2aa1a9b18bcece330955e53a3798
102244653 <30612195+102244653@users.noreply.github.com>
2018-12-21 18:36:58 +0800

    优化json解析

--- src/main/java/Utils/CheckResult.java
+++ src/main/java/Utils/CheckResult.java
@@ -2,25 +2,27 @@ package Utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import com.alibaba.fastjson.JSONObject;
+import Utils.GetJsonByJpath;
 import java.util.HashMap;
 
 public class CheckResult {
 
     private static Logger logger = LoggerFactory.getLogger(CheckResult.class);
     /**
-     * object格式：比较方式[字段名：预期结果]  ==  equals[key:vlaue]
+     * object格式：比较方式{字段名：预期结果}  ==  equals{key:vlaue}
      */
     public static boolean checkresult(String response,String exceptresult) throws Exception {
         int countflag=0;
         if(response.isEmpty() && exceptresult.isEmpty()) return false;
-        HashMap mapresult= (HashMap<String, String>) new AnylistJson().AnylistJson(response);
+        //HashMap mapresult= (HashMap<String, String>) new AnylistJson().AnylistJson(response);//旧方法解析json
+        JSONObject responseJson = JSON.parseObject(response);        
         String[] object= new CutStrUtil().SplitByIndex(exceptresult,";");
         for(int i=0;i<object.length;i++){
             String text=object[i];
-            int point1=text.trim().indexOf("[");
+            int point1=text.trim().indexOf("{");
             int point2=text.trim().indexOf(":");
-            int point3=text.trim().indexOf("]");
+            int point3=text.trim().indexOf("}");
             String type="";
             if(point1!=0){
                 type=text.substring(0,point1);
@@ -36,7 +38,8 @@ public class CheckResult {
             switch (type.trim()){
                 case "equals":
                     try {
-                        String _value1= (String) mapresult.get(key);
+                        //String _value1= (String) mapresult.get(key);
+                        String _value1=GetJsonByJpath.getValueByJPath(key);
                         if(!_value1.equals(except)){
                             countflag++;
                             logger.info(text+"  结果校对错误");
@@ -49,7 +52,8 @@ public class CheckResult {
                     break;
                 case "contain":
                     try {
-                         String _value2= (String) mapresult.get(key);
+                         //String _value2= (String) mapresult.get(key);
+                         String _value2=GetJsonByJpath.getValueByJPath(key);
                          if(!_value2.contains(except)){
                              countflag++;
                             logger.info(text+"  结果校对错误");
@@ -65,7 +69,8 @@ public class CheckResult {
                     break;
                 case "start":
                     try {
-                        String _value3= (String) mapresult.get(key);
+                        //String _value3= (String) mapresult.get(key);
+                         String _value3=GetJsonByJpath.getValueByJPath(key);
                         if(!_value3.startsWith(except)){
                             countflag++;
                             logger.info(text+"  结果校对错误");
@@ -78,7 +83,8 @@ public class CheckResult {
                     break;
                 case "end":
                     try {
-                        String _value4= (String) mapresult.get(key);
+                        //String _value4= (String) mapresult.get(key);
+                        String _value4=GetJsonByJpath.getValueByJPath(key);
                         if(!_value4.endsWith(except)){
                             countflag++;
                             logger.info(text+"  结果校对错误");
