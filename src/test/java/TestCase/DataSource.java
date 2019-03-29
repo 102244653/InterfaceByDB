707056c73a8ee38af8026a4130d77ac070b2cb84
102244653 <30612195+102244653@users.noreply.github.com>
2019-03-29 11:42:51 +0800

    1
    1

--- src/test/java/TestCase/DataSource.java
+++ src/test/java/TestCase/DataSource.java
@@ -44,33 +44,4 @@ public class DataSource {
         }
         return suitcase;
     }
-
-
-
-    //过滤二维数组空元素,未使用
-    public static Object[][] deletenull(Object[][] objects){
-        int firstDimesion = objects.length;//二维数组初始长度
-        for(int i=0; i<objects.length; i++){
-            if(objects[i].length == 1 && objects[i][0] == null)//过滤掉一维为空的元素后的长度
-                firstDimesion --;
-        }
-        result = new Object[firstDimesion][];//返回的一维数组均为非空
-        firstDimesion = 0;//一维初始化从0开始
-        for(int i=0; i<objects.length; i++) {
-            if (objects[i].length == 1 && objects[i][0] == null ) {//为空则直接跳过
-                continue;
-            }
-            temp = new Object[]{objects[i][0], objects[i][1]};//存储非null元素
-            result[firstDimesion] = new Object[2];//第一个值含两个元素
-            for (int k = 0; k < 2; k++){
-                result[firstDimesion][k] = temp[k];//给两个元素赋值
-            }
-            if(i!=objects.length){
-                firstDimesion ++; //长度增一，初始化下一个数组
-            }
-        }
-        return result;
-    }
-    private static Object[] temp; //temp存储非null元素
-    private static Object[][] result;//最终结果
 }
